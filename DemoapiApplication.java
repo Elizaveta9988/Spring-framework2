17a789c19047d2263bec493b7ccb921d50ac53c2
Liza <liza@iMac-Liza.local>
2024-01-27 19:37:59 +0300

    New document

---
+++ DemoapiApplication.java
@@ -0,0 +1,6 @@
+public class DemoapiApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(DemoapiApplication.class, args);
+	}
+
+}
---
+++ NotificationService.java
@@ -0,0 +1,8 @@
+public class NotificationService {
+    public void notifyUser(User user) {
+        System.out.println("A new user has been created: " + user.getName());
+    }
+    public void notifyUserReg(User user) {
+        System.out.println("A new user has been registered: " + user.getName());
+    }
+}
